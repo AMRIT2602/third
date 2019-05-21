b2722045f0bba9a4a85863e3f98e764a5a993d97
HP <HP@DESKTOP-6SUN5DR>
2019-05-22 11:55:58 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace third
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Enter  Number");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter  Second Number");
+            b= int.Parse(Console.ReadLine());
+
+            Console.WriteLine( a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
